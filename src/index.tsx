ff2f3c9f59b3eac98740cd9c4985df2cbedce3d1
Vodopyanov Egor <egorvodopyanov@gmail.com>
2020-10-26 20:01:59 +0300

    use exodus reanimated module publish "module" bundle only

--- src/index.tsx
+++ src/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 import { Dimensions, Platform, View, LayoutChangeEvent } from 'react-native'
-import Animated from 'react-native-reanimated'
+import Animated from '@exodus/react-native-reanimated'
 import {
   PanGestureHandler,
   TapGestureHandler,
